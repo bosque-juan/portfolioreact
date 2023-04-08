9ad7247973c2c9f190865d0ead9f1c9c5ba2fc03
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-04-08 12:42:19 -0300

    nav-someStyles

--- src/presentational/Nav.jsx
+++ src/presentational/Nav.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Style from "./Nav.module.css";
 
 export default function Nav() {
   return (
-    <div>
-        <NavLink to='/home'>Home</NavLink>
-        <NavLink to='/about'>About</NavLink>
-        <NavLink to='/skills'>Skills</NavLink>
-        <NavLink to='/portfolio'>Portfolio</NavLink>
-        <NavLink to='/cantact'>Contact</NavLink>
+    <div  className={Style.head}>
+      <div className={Style.name}>
+        <ul>Juan Bosque</ul>
+      </div>
+      <div className={Style.links}>
+        <ul>
+          <NavLink to="/home">Home</NavLink>
+        </ul>
+        <ul>
+          <NavLink to="/about">About</NavLink>
+        </ul>
+        <ul>
+          <NavLink to="/skills">Skills</NavLink>
+        </ul>
+        <ul>
+          <NavLink to="/portfolio">Portfolio</NavLink>
+        </ul>
+        <ul>
+          <NavLink to="/cantact">Contact</NavLink>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
