5bc065ee6ff0172622ed36ecc5826a1edcc67b4c
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-06-26 07:21:39 -0300

    about text

--- src/presentational/About.jsx
+++ src/presentational/About.jsx
@@ -9,31 +9,27 @@ export default function About() {
         <main className={Style.mainAbout}>About</main>
 
         <div className={Style.containerAbout}>
-         <div className={Style.imgContainer1}>
-
-          <img className={Style.imageAbout} src={img} alt="" />
-         </div>
+          <div className={Style.imgContainer1}>
+            <img className={Style.imageAbout} src={img} alt="" />
+          </div>
 
           <div className={Style.about}>
             <h2>I am Juan Bosque</h2>
             <span>Full Stack Web Developer</span>
             <p className={Style.abouText}>
-              I am enthusiastic, proactive, I like to always be learning new
-              tools, improve what I have done, deepen what I have learned. I
-              have full time availability and the possibility of immediate
-              application.
+              Passionate about what I do, I like to be learning, innovating,
+              doing something different, dedication, helping where I can. I like
+              solving situations
             </p>
 
             <div className={Style.iconContainer}>
               <a href="https://www.linkedin.com/in/juan-bosque">
                 <i>
-         
                   <AiFillLinkedin className={Style.icon} />{" "}
                 </i>
               </a>
               <a href="https://www.github.com/juanchos85">
                 <i>
-         
                   <AiFillGithub className={Style.icon} />{" "}
                 </i>
               </a>
