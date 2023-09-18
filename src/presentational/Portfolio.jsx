fc8b78f7df9591e9913848c058c39cc810de5e12
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-09-18 20:30:28 -0300

    url food

--- src/presentational/Portfolio.jsx
+++ src/presentational/Portfolio.jsx
@@ -26,7 +26,7 @@ export default function Portfolio() {
           <div className={Style.imgContainer}>
         
               <img className={Style.imgPortfolio} src={recipess} alt="" /> 
-            <a href="https://recipes-responsive.vercel.app/">
+            <a href="https://recipes-food-app-responsive.vercel.app/">
             <h5 className={Style.imgTitle}>
               Food Recipes App
               <br/>  to make your food!
