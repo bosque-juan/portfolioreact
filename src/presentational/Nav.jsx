cc84316a710cc85c4629f57973b5e61a49572451
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-09 12:03:14 -0300

    Nav responsive

--- src/presentational/Nav.jsx
+++ src/presentational/Nav.jsx
@@ -1,64 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Style from "./Nav.module.css";
 
 export default function Nav() {
   const [activeLink, setActiveLink] = useState("home");
-
+  function Close() {
+    setMenu(!menu);
+  }
   const handleLinkClick = (link) => {
     setActiveLink(link);
+    small && Close();
   };
+  const [menu, setMenu] = useState(true);
+
+  const [small, setSmall] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth <= 700) {
+      setMenu(false);
+
+      setSmall(true);
+    } else {
+      setMenu(true);
+      // setShowButton(false);
+      setSmall(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [small]);
 
   return (
     <div className={Style.headNav}>
       <div className={Style.logo}>JB</div>
-      <nav className={Style.links}>
-        <span>
-          <a
-            className={activeLink === "home" ? Style.active : ""}
-            href="#home"
-            onClick={() => handleLinkClick("home")}
-          >
-            Home
-          </a>
-        </span>
-        <span>
-          <a
-            href="#about"
-            className={activeLink === "about" ? Style.active : ""}
-            onClick={() => handleLinkClick("about")}
-          >
-            About
-          </a>
-        </span>
-        <span>
-          <a
-            href="#skills"
-            className={activeLink === "skills" ? Style.active : ""}
-            onClick={() => handleLinkClick("skills")}
-          >
-            Skills
-          </a>
-        </span>
-        <span>
-          <a
-            href="#portfolio"
-            className={activeLink === "portfolio" ? Style.active : ""}
-            onClick={() => handleLinkClick("portfolio")}
-          >
-            Portfolio
-          </a>
-        </span>
-        <span>
-          <a
-            href="#contact"
-            className={activeLink === "contact" ? Style.active : ""}
-            onClick={() => handleLinkClick("contact")}
-          >
-            Contact
-          </a>
-        </span>
-      </nav>
+      {menu && (
+        <nav className={Style.links}>
+          <span>
+            <a
+              className={activeLink === "home" ? Style.active : ""}
+              href="#home"
+              onClick={() => handleLinkClick("home")}
+            >
+              Home
+            </a>
+          </span>
+          <span>
+            <a
+              href="#about"
+              className={activeLink === "about" ? Style.active : ""}
+              onClick={() => handleLinkClick("about")}
+            >
+              About
+            </a>
+          </span>
+          <span>
+            <a
+              href="#skills"
+              className={activeLink === "skills" ? Style.active : ""}
+              onClick={() => handleLinkClick("skills")}
+            >
+              Skills
+            </a>
+          </span>
+          <span>
+            <a
+              href="#portfolio"
+              className={activeLink === "portfolio" ? Style.active : ""}
+              onClick={() => handleLinkClick("portfolio")}
+            >
+              Portfolio
+            </a>
+          </span>
+          <span>
+            <a
+              href="#contact"
+              className={activeLink === "contact" ? Style.active : ""}
+              onClick={() => handleLinkClick("contact")}
+            >
+              Contact
+            </a>
+          </span>
+        </nav>
+      )}
+      <div className={Style.mobile}>
+        <i
+          className={menu ? "fas fa-times" : "fas fa-bars"}
+          onClick={() => setMenu(!menu)}
+        ></i>
+      </div>
     </div>
   );
 }
-
