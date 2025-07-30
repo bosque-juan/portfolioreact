e0d7a8b7ed20c34e1723ce94f95bb5602dd88dd8
juan <juan_bosque@hotmail.com>
2025-07-30 09:34:14 -0300

    change: about description

--- src/presentational/About.jsx
+++ src/presentational/About.jsx
@@ -11,19 +11,16 @@ export default function About() {
         <main className={Style.mainAbout}>About</main>
         <div className={Style.containerAbout}>
           <div className={Style.imgContainer1}>
-            <img className={Style.imageAbout} src={img} alt="Juan Bosque Image"/>
+            <img className={Style.imageAbout} src={img} alt="Juan Bosque Image" />
           </div>
 
           <div className={Style.about}>
             <h2>Juan Bosque</h2>
             <span>Full Stack Web Developer</span>
             <p className={Style.abouText}>
-              Experience one, more than a year on this field. Responsible,
-              committed. I like having challenges, achieving goals with the
-              team. I like innovation, developing interactive web pages; with
-              special attention to user experience; making web and responsives
-              wev apps. I follow the last tendence and I am always learning new
-              tools or deepen wich I know
+              I’m a full stack web developer. I’m passionate about learning new practices, improving every day, and applying what I learn.
+              I had experience working as a full stack developer at Shaper US, where I grew both technically and professionally.
+              Currently, I’m studying Programming at the National Technological University to continue strengthening my knowledge.
             </p>
 
             <ul className={Style.iconContainer}>
