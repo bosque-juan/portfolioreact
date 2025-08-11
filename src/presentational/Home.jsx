ceb0687247ff70f06a39af0c9c22b21be6237898
juan <juan_bosque@hotmail.com>
2025-08-11 10:40:27 -0300

    fix:Spanish version element spaces

--- src/presentational/Home.jsx
+++ src/presentational/Home.jsx
@@ -2,26 +2,29 @@ import Style from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div  className={Style.head}>
-      <a href="#about">
-        <i>
-          <h6>Scroll Down</h6>
-        </i>
-      </a>
-      <div className={Style.containerHome}>
-        <div className={Style.greeting}>
-          <span>JUAN</span>
-          <br />
-          BOSQUE
-          <h5 className={Style.subtitle}>FULL STACK WEB DEVELOPER</h5>
+    <div className={Style.head}>
+      <article className={Style.home_wrapper}>
+        <a href="#about">
+          <i>
+            <h6>Scroll Down</h6>
+          </i>
+        </a>
+        <div className={Style.containerHome}>
+          <div className={Style.greeting}>
+            <span>JUAN</span>
+            <br />
+            BOSQUE
+            <h5 className={Style.subtitle}>FULL STACK WEB DEVELOPER</h5>
+          </div>
+
+          <div>
+            <h3 className={Style.mainTitle}>
+              Welcome to my personal portfolio!
+            </h3>
+          </div>
         </div>
 
-        <div>
-          <h3 className={Style.mainTitle}>
-            Welcome to my personal portfolio!
-          </h3>
-        </div>
-      </div>
+      </article>
     </div>
   );
 }
